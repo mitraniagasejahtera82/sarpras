7b2c756852945dc6e945a15391dec021237cf739
zul <mitraniagasejahtera82@gmail.com>
2026-02-16 13:11:18 +0700

    Clean gitignore, remove db.sqlite3 and pycache

--- config/settings.py
+++ config/settings.py
@@ -10,18 +10,27 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # =====================================
 # SECURITY
 # =====================================
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key-local")
 
+
+#DEBUG = True
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.environ.get(
     "ALLOWED_HOSTS",
-    "sarpras.onrender.com"
+    "127.0.0.1,localhost,sarpras.onrender.com"
 ).split(",")
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://sarpras.onrender.com"
+    "http://127.0.0.1:8000",
+    "http://localhost:8000",
+    "https://sarpras.onrender.com",
 ]
+
+if not DEBUG:
+    CSRF_COOKIE_SECURE = True
+    SESSION_COOKIE_SECURE = True
+
 
 # =====================================
 # APPLICATION DEFINITION
